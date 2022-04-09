04087db5e7da132417c6cb5d6bf4ec1bf1b56538
single-right-quote <34298117+single-right-quote@users.noreply.github.com>
2022-04-09 06:36:23 +0000

    Make account string validation consistent with mention processing
    We previously matched on one-character domains and domains ending with
    `[\.-]`, allowing `from:@a@a` and `from:@a@a-` searches to cause an
    account lookup. This commit will raise a syntax error in both cases, as
    MENTION_RE would never match them.

--- app/models/concerns/account_finder_concern.rb
+++ app/models/concerns/account_finder_concern.rb
@@ -38,7 +38,15 @@ module AccountFinderConcern
     end
   end
 
-  ACCOUNT_STRING_RE = /^@?(?<username>#{Account::USERNAME_RE})(?:@(?<domain>[[:word:]\.\-]+))?$/i
+  # TODO: where should this go?
+  #
+  # this is adapted from MENTION_RE to
+  #   + capture only a mention,
+  #   + not require the mention to begin with an @,
+  #   + not match if there is anything surrounding the mention, and
+  #   + add named subgroup matches
+  # it would be ideal to explicitly refer to MENTION_RE, or a more fundamental regexp that we refactor MENTION_RE to incorporate
+  ACCOUNT_STRING_RE = /^@?(?<username>#{Account::USERNAME_RE})(?:@(?<domain>[[:word:]\.\-]+[[:word:]]+))?$/i
 
   class AccountFinder
     attr_reader :username, :domain
